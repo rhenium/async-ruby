343f5e258a651f77ca03cb5605a4ac80d3402d61
Kazuki Yamaguchi <k@rhe.jp>
2015-11-20 13:13:47 +0900

    if

--- lib/async.rb
+++ lib/async.rb
@@ -4,35 +4,54 @@ require "async/ext"
 require "pp"
 
 module Async
-  def self.transform(array)
-    # TODO: is really the await?
-    await_index = array[13].index { |ins|
-      ins.is_a?(Array) &&
-        (ins[0] == :opt_send_without_block || ins[0] == :send) &&
-        ins[1][:mid] == :await
-    }
-
-    pp array
-    if await_index
-      transform_(array, await_index)
-    end
+  def self.transform(ary)
+    pp ary
+    ary[13] = transform1(ary, ary[13])
+    pp ary
   end
 
-  def self.transform_(ary, ai)
-    line_number = ary[13].take(ai).reverse.find { |i| i.is_a?(Fixnum) }
+  def self.find_ins_o(part, offset, size, insns, val = [])
+    part[offset, size || part.size].index { |ins|
+      ins.is_a?(Array) &&
+        insns.include?(ins[0]) &&
+        val.each_with_index.all? { |v, i|
+          !v ||
+            (lv = ins[i + 1]) && (lv.is_a?(Hash) && v.is_a?(Hash) ? lv >= v : lv == v) } }
+  end
 
-    inner_end_i = ary[13].rindex { |i| i.is_a?(Array) && i != [:trace, 16] && i != [:trace, 512] && i != [:leave] }
-    inner_body = ary[13].slice!(ai..inner_end_i)
-    inner_body.shift(1) # remove send :await
+  def self.transform1(ary, part)
+    p begin_if_i = find_ins_o(part, 0, nil, [:branchif, :branchunless])
+    p first_await_i = find_ins_o(part, 0, nil, [:send, :opt_send_without_block], [{ mid: :await }])
+    end_i = part.rindex { |i| i.is_a?(Array) && i != [:trace, 16] && i != [:trace, 512] && i != [:leave] }
 
-    inner_ctable = []
-    ary[12].reject! { |cc| # [type, iseq?, start, end, cont, sp]
-      if cc[2..4].all? { |c| inner_body.include?(c) }
-        inner_ctable << cc
-      elsif cc[2..4].any? { |c| inner_body.include?(c) }
-        raise SyntaxError, "async currently doesn't support catching across await-kw"
-      end
-    }
+    return part unless first_await_i
+
+    if !begin_if_i || first_await_i < begin_if_i
+      part[first_await_i, end_i] = wrap_async(ary, part[first_await_i..end_i])
+    else
+      end_if_o = part.index(part[begin_if_i][1]) - begin_if_i # label
+      await_o = first_await_i - begin_if_i
+      # else?
+      part[begin_if_i + await_o, end_if_o - await_o] = wrap_async(ary, part[begin_if_i + await_o, end_if_o - await_o] + part[begin_if_i + end_if_o + 1..end_i])
+    end
+
+    part
+  end
+
+  def self.wrap_async(ary, part)
+    line_number = 999 # TODO
+    # line_number = ary[13].take(ai).reverse.find { |i| i.is_a?(Fixnum) }
+
+    inner_body = part.drop(1)
+
+    # inner_ctable = []
+    # ary[12].reject! { |cc| # [type, iseq?, start, end, cont, sp]
+    #   if cc[2..4].all? { |c| inner_body.include?(c) }
+    #     inner_ctable << cc
+    #   elsif cc[2..4].any? { |c| inner_body.include?(c) }
+    #     raise SyntaxError, "async currently doesn't support catching across await-kw"
+    #   end
+    # }
 
     inner = [
       *ary.take(4),
@@ -44,7 +63,7 @@ module Async
       :block,
       [:"#{"await_result"}"], # param
       { lead_num: 1 },
-      inner_ctable,
+      [], # inner_ctable,
       [
         line_number,
         [:trace, 256], # RUBY_EVENT_B_CALL
@@ -56,15 +75,17 @@ module Async
 
     fixlocal(inner, 0)
     inner[13].insert(2, [:getlocal_OP__WC__0, 2],
-                        [:opt_send_without_block, { mid: :result, flag: 0, orig_argc: 0 }, false])
+                     [:opt_send_without_block, { mid: :result, flag: 0, orig_argc: 0 }, false])
 
-    transform(inner) # next await in same level
+    # transform(inner) # next await in same level
 
     #    ... self task -> swap
     # -> ... task self -> pop
     # -> ... task -> send
     # -> ...
-    ary[13].insert(ai, [:swap], [:pop], [:send, { mid: :continue_with, flag: 4, orig_argc: 0 }, false, inner])
+    part.insert(0, [:swap], [:pop], [:send, { mid: :continue_with, flag: 4, orig_argc: 0 }, false, inner], [:leave])
+
+    part
   end
 
   def self.fixlocal(ary, level)
